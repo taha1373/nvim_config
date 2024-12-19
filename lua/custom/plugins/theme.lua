4e9fe61568a02f22de5c2330c8be0f9b13419ce3
Taha Rajabzadeh (C) <trajabzadeh@TRajabzadeh-MBP02.local>
2024-12-18 20:08:05 -0800

    add theme to custom plugins

---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,21 @@
+return {
+  'sainnhe/gruvbox-material',
+  priority = 1000, -- Load this plugin before others
+  config = function()
+    -- Set gruvbox-material to dark mode
+    vim.g.gruvbox_material_background = 'medium'
+    -- Enable the material palette
+    vim.g.gruvbox_material_palette = 'material'
+    -- Disable bold fonts (optional)
+    vim.g.gruvbox_material_bold = false
+    -- Set better contrast for sidebars, popups, and floats
+    vim.g.gruvbox_material_visual = 'reverse'
+    vim.g.gruvbox_material_sign_column_background = 'none'
+    vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+
+    -- Apply the colorscheme
+    vim.cmd.colorscheme 'gruvbox-material'
+    -- Link Python docstring highlight group to Comment
+    vim.api.nvim_set_hl(0, '@string.documentation.python', { link = 'Comment' })
+  end,
+}
